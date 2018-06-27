1eee00466317c74ed75f4d2851bdfca7eb53d6df
Hlib Kanyshchev <hlib_kanyshchev@epam.com>
2018-06-27 17:15:50 +0300

    dynamic search inputs fix

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule],
-  exports: [MatButtonModule, MatCheckboxModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatAutocompleteModule, MatFormFieldModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatAutocompleteModule, MatFormFieldModule],
 })
 export class MaterialModule { }
