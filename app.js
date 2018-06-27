0c3d8503df80051b67797a6f4f21c882870e40d6
Hlib Kanyshchev <hlib_kanyshchev@epam.com>
2018-06-27 13:04:24 +0300

    initial bootstrap styles

--- app.js
+++ app.js
@@ -5,83 +5,100 @@ const app = express()
 
 const port = process.env.PORT || 3000;
 
-const mockData = [{
-    "id": 1,
-    "from": "Lviv",
-    "to": "Kiev",
-    "date": "11/20/2017",
-    "cost": "$0.92",
-    "company": "Jaxspan"
-  }, {
-    "id": 2,
-    "from": "Lviv",
-    "to": "Odessa",
-    "date": "1/11/2018",
-    "cost": "$7.42",
-    "company": "Blogpad"
-  }, {
-    "id": 3,
-    "from": "Hulutao",
-    "to": "Cuozheqiangma",
-    "date": "7/19/2017",
-    "cost": "$9.95",
-    "company": "Zoombox"
-  }, {
-    "id": 4,
-    "from": "Panshi",
-    "to": "Picoto",
-    "date": "8/30/2017",
-    "cost": "$1.60",
-    "company": "DabZ"
-  }, {
-    "id": 5,
-    "from": "Chak",
-    "to": "Köln",
-    "date": "1/5/2018",
-    "cost": "$9.15",
-    "company": "Quimba"
-  }, {
-    "id": 6,
-    "from": "Lviv",
-    "to": "Kiev",
-    "date": "10/29/2017",
-    "cost": "$6.15",
-    "company": "Rhynyx"
-  }, {
-    "id": 7,
-    "from": "Cayenne",
-    "to": "Mahdalynivka",
-    "date": "2/26/2018",
-    "cost": "$9.03",
-    "company": "Feedbug"
-  }, {
-    "id": 8,
-    "from": "Chixi",
-    "to": "Guanghou",
-    "date": "1/18/2018",
-    "cost": "$5.69",
-    "company": "Skyvu"
-  }, {
-    "id": 9,
-    "from": "Fort Wayne",
-    "to": "Monte Azul Paulista",
-    "date": "10/18/2017",
-    "cost": "$5.40",
-    "company": "Mydo"
-  }, {
-    "id": 10,
-    "from": "El Cafetal",
-    "to": "Guanambi",
-    "date": "8/20/2017",
-    "cost": "$6.73",
-    "company": "Gabvine"
-  }];
+// const mockData = [{
+//     "id": 1,
+//     "from": "Lviv",
+//     "to": "Kiev",
+//     "date": "11/20/2017",
+//     "cost": "$0.92",
+//     "company": "Jaxspan"
+//   }, {
+//     "id": 2,
+//     "from": "Lviv",
+//     "to": "Odessa",
+//     "date": "1/11/2018",
+//     "cost": "$7.42",
+//     "company": "Blogpad"
+//   }, {
+//     "id": 3,
+//     "from": "Hulutao",
+//     "to": "Cuozheqiangma",
+//     "date": "7/19/2017",
+//     "cost": "$9.95",
+//     "company": "Zoombox"
+//   }, {
+//     "id": 4,
+//     "from": "Panshi",
+//     "to": "Picoto",
+//     "date": "8/30/2017",
+//     "cost": "$1.60",
+//     "company": "DabZ"
+//   }, {
+//     "id": 5,
+//     "from": "Chak",
+//     "to": "Köln",
+//     "date": "1/5/2018",
+//     "cost": "$9.15",
+//     "company": "Quimba"
+//   }, {
+//     "id": 6,
+//     "from": "Lviv",
+//     "to": "Kiev",
+//     "date": "10/29/2017",
+//     "cost": "$6.15",
+//     "company": "Rhynyx"
+//   }, {
+//     "id": 7,
+//     "from": "Cayenne",
+//     "to": "Mahdalynivka",
+//     "date": "2/26/2018",
+//     "cost": "$9.03",
+//     "company": "Feedbug"
+//   }, {
+//     "id": 8,
+//     "from": "Chixi",
+//     "to": "Guanghou",
+//     "date": "1/18/2018",
+//     "cost": "$5.69",
+//     "company": "Skyvu"
+//   }, {
+//     "id": 9,
+//     "from": "Fort Wayne",
+//     "to": "Monte Azul Paulista",
+//     "date": "10/18/2017",
+//     "cost": "$5.40",
+//     "company": "Mydo"
+//   }, {
+//     "id": 10,
+//     "from": "El Cafetal",
+//     "to": "Guanambi",
+//     "date": "8/20/2017",
+//     "cost": "$6.73",
+//     "company": "Gabvine"
+//   }];
+
+// app.get('/search', (req, res) => {
+//     var parsedResponse = mockData.filter((obj) => obj.from.toLowerCase() === req.query.from.toLowerCase() && obj.to.toLowerCase() === req.query.to.toLowerCase());
+//     res.send(parsedResponse);
+// });
+
+var myLogger = function (req, res, next) {
+  console.log('LOGGED')
+  console.log(req.query.from);
+  
+  next()
+}
+
+app.use(myLogger)
+
 
 app.get('/search', (req, res) => {
-    var parsedResponse = mockData.filter((obj) => obj.from.toLowerCase() === req.query.from.toLowerCase() && obj.to.toLowerCase() === req.query.to.toLowerCase());
-    res.send(parsedResponse);
+  origin = req.query.from;
+  destination = req.query.to;
+  res.send(`origin destination`)
+  
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => console.log(`Starting the server at port ${port}`));
+app.listen(port, () => console.log(`Listening to port ${port}`));
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { DataService } from './data.service';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,24 +12,53 @@ export class AppComponent {
 
   constructor(private dataService: DataService) {}
 
-  searchData;
-  responseData;
-  dataLength;
+  originFull;
+  destinationFull;
+  origin;
+  currency;
+  results;
 
+  errResponse = false;
+  isLoaded = false;
 
   onSubmit(form: NgForm) {
-    this.searchData = form.value;
+    this.originFull = form.value.from;
+    this.destinationFull = form.value.to;
+    this.dataService.getData(form.value);
+    setTimeout(() => {
+      if (this.dataService.response) {
+        this.origin = this.dataService.response.origin;
+        this.currency = this.dataService.response.currency;
+        this.results = this.dataService.response.results;
+        this.isLoaded = true;
+      } else {
+        this.errResponse = true;
+        this.isLoaded = true;
+      }
+    }, 1000);
     
-    this.dataService.getData(form.value)
-      .subscribe((response) => {
-        this.responseData = response.json();
-        this.dataLength = response.json().length;
-      });
-      form.reset();
+    form.reset();
+  }
+
+  modalClose() {
+    console.log("closed");
+    this.errResponse = false;
+    this.isLoaded = false;
   }
 
   printPage() {
     window.print();
   }
-  
+
 }
+
+// {
+//   "origin" : "MUC",
+//   "currency" : "EUR",
+//   "results" : [ {
+//     "destination" : "BKK",
+//     "departure_date" : "2018-06-26",
+//     "price" : "411.44",
+//     "airline" : "TG"
+//   } ]
+// }
